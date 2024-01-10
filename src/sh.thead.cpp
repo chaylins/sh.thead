1cf15753d5647d4e7b3d30dd36df04938c9358b5
chaylins <craig.haylins@gmail.com>
2024-01-10 21:56:56 +0100

    initial commit

---
+++ src/sh.thead.cpp
@@ -0,0 +1,12 @@
+﻿// sh.thead.cpp : Defines the entry point for the application.
+//
+
+#include "..\include\sh.thead.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
